9cf85fabe3c5577ed0074c4e7ff67ec3bf709634
Federico Silvestri <federico.silvestri@vedrai.com>
2020-10-25 17:14:03 +0100

    Fixed importing

--- gooutsafe/models/customer.py
+++ gooutsafe/models/customer.py
@@ -1,11 +1,11 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+from .user import User
 
-from gooutsafe.database import db
-from user.py import User
+from gooutsafe import db
 
 
 class Customer(User):
-    __tablename__ = 'customer'
+    __tablename__ = 'Customer'
+
     firstname = db.Column(db.Unicode(128))
     lastname = db.Column(db.Unicode(128))
     date_of_birth = db.Column(db.DateTime)
@@ -15,35 +15,34 @@ class Customer(User):
         super(User, self).__init__(*args, **kw)
         self._authenticated = False
 
-
     @property
     def firstname(self):
         return self.firstname
-    
+
     @firstname.setter
-    def firstname (self, name):
+    def firstname(self, name):
         self.firstname = name
 
     @property
     def lastname(self):
         return self.lastname
-    
+
     @lastname.setter
-    def lastname (self, name):
+    def lastname(self, name):
         self.lastname = name
 
     @property
     def date_of_birth(self):
         return self.date_of_birth
-    
+
     @date_of_birth.setter
-    def date_of_birth (self, dateBirth):
+    def date_of_birth(self, dateBirth):
         self.date_of_birth = dateBirth
 
     @property
     def health_status(self):
         return self.health_status
-    
+
     @health_status.setter
-    def health_status (self, status):
+    def health_status(self, status):
         self.health_status = status
