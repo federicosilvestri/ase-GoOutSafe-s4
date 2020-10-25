a53d5a67c95e64f78a0476a05106fe4d27b1ce84
LeoCal4 <linkindayd@gmail.com>
2020-10-25 17:29:07 +0100

    Initial restaurant model, added pytest

--- gooutsafe/models/restaurant.py
+++ gooutsafe/models/restaurant.py
@@ -2,10 +2,23 @@ from gooutsafe.database import db
 
 
 class Restaurant(db.Model):
+    
     __tablename__ = 'restaurant'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # TODO: add owner relationship when its model is created
     name = db.Column(db.Text(100))
     likes = db.Column(db.Integer)
     lat = db.Column(db.Float)
     lon = db.Column(db.Float)
     phone = db.Column(db.Integer)
+    menu_type = db.Column(db.Text(100))
+    is_open = db.Column(db.Boolean, default=False)
+    # TODO: add ratings relationship when their model is created
+    # TODO: add availability relationship when their model is created
+
+    def __init__(self, name, lat, lon, phone, menu_type):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+        self.phone = phone
+        self.menu_type = menu_type
--- gooutsafe/models/tests/test_restaurant.py
+++ gooutsafe/models/tests/test_restaurant.py
@@ -5,19 +5,13 @@ from gooutsafe.models.restaurant import Restaurant
 class TestRestaurant(unittest.TestCase):
 
     def test_rest_init(self):
-        rest = Restaurant('./gooutsafe/models/tests/rest0.txt')
-        name = rest.name
-        tables = rest.tables
-
-        self.assertEqual(name, 'ASEPizza')
-        self.assertEqual(len(tables), 3)
-        self.assertEqual(tables[0].name, 'red')
-        self.assertEqual(tables[0].capacity, 10)
-        self.assertEqual(tables[1].name, 'yellow')
-        self.assertEqual(tables[1].capacity, 2)
-        self.assertEqual(tables[2].name, 'pink')
-        self.assertEqual(tables[2].capacity, 3)
-
+        restaurant = Restaurant('Quello Buono', 10, 20, 55555555, 'Vegetarian')
+        self.assertEqual(restaurant.name, 'Quello Buono')
+        self.assertEqual(restaurant.lat, 10)
+        self.assertEqual(restaurant.lon, 20)
+        self.assertEqual(restaurant.phone, 55555555)
+        self.assertEqual(restaurant.menu_type, 'Vegetarian')
+        
 
 if __name__ == '__main__':
     unittest.main()
