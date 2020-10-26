71eb0d0526322727d6d1e66f0e62238626e8e447
Claire-gip <chiara_boni@outlook.it>
2020-10-26 16:12:44 +0100

    registrations and (almost) login

--- gooutsafe/views/auth.py
+++ gooutsafe/views/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect
-from flask_login import (login_user, logout_user)
+from flask_login import (login_user, logout_user, current_user, login_user, login_required)
 
 from gooutsafe import db
 from gooutsafe.forms import LoginForm
@@ -15,14 +15,32 @@ def login():
         email, password = form.data['email'], form.data['password']
         q = db.session.query(User).filter(User.email == email)
         user = q.first()
-        print(q.first().id)
+
         if user is not None and user.authenticate(password):
             login_user(user)
-            return redirect('/')
+
+            if (user.type == 'operator'):
+                return redirect('/operator')
+            else:
+                return redirect('/profile')
+
     return render_template('login.html', form=form)
 
 
+#TODO: put the current_user
+@auth.route('/profile', methods=['GET', 'POST'])
+def profile():
+    return render_template('customer_profile.html')
+
+#TODO: put the current_user
+@auth.route('/operator', methods=['GET', 'POST'])
+def operator():
+    return render_template('operator_profile.html')
+
+
 @auth.route("/logout")
+@login_required
 def logout():
     logout_user()
+    user.authenticated = False
     return redirect('/')
