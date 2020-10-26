9a5478ed94676de8becd6bcfa97751a5742c04bf
Paolo Murgia <62264946+Musca23@users.noreply.github.com>
2020-10-26 11:29:49 +0100

    removed inherited attributes

--- gooutsafe/models/operator.py
+++ gooutsafe/models/operator.py
@@ -8,11 +8,6 @@ class Operator(User):
     __tablename__ = 'Operator'
 
     id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
-    email = db.Column(db.Unicode(128), nullable=False)
-    password = db.Column(db.Unicode(128))
-    is_active = db.Column(db.Boolean, default=True)
-    is_admin = db.Column(db.Boolean, default=False)
-    is_anonymous = False
 
     __mapper_args__ = {
         'polymorphic_identity':'operator',
@@ -21,18 +16,6 @@ class Operator(User):
     def __init__(self, *args, **kw):
         super(Operator, self).__init__(*args, **kw)
         self._authenticated = False
-
-    def set_password(self, password):
-        self.password = generate_password_hash(password)
-
-    @property
-    def is_authenticated(self):
-        return self._authenticated
-
-    def authenticate(self, password):
-        checked = check_password_hash(self.password, password)
-        self._authenticated = checked
-        return self._authenticated
 
     def get_id(self):
         return self.id
