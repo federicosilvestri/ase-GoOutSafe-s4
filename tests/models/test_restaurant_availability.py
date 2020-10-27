ec8219c4f5426779e660495c90492ac4712bd63a
LeoCal4 <linkindayd@gmail.com>
2020-10-27 22:30:48 +0100

    Added Restaurant Availability model

---
+++ tests/models/test_restaurant_availability.py
@@ -0,0 +1,18 @@
+import unittest
+from datetime import datetime
+
+class TestRestaurantAvailability(unittest.TestCase):
+
+    def setUp(self):
+        from gooutsafe import create_app
+        create_app('config.TestConfig')
+
+        from gooutsafe.models import restaurant
+        from gooutsafe.models import restaurant_availability
+        self.restaurant = restaurant
+        self.availability = restaurant_availability
+
+
+    def test_init(self):
+        _restaurant = self.restaurant.Restaurant('Quello Buono', 10, 20, 55555555, 'Vegetarian')
+        _avail = self.availability.RestaurantAvailability(_restaurant.__id, datetime.now(), datetime.now())
