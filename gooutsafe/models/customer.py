a3d7a2792655935ded85d6573efabb59ec9b3abd
Federico Silvestri <federico.silvestri@vedrai.com>
2020-10-27 22:25:31 +0100

    Fixed properties

--- gooutsafe/models/customer.py
+++ gooutsafe/models/customer.py
@@ -8,7 +8,7 @@ class Customer(User):
     id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
     firstname = db.Column(db.Unicode(128))
     lastname = db.Column(db.Unicode(128))
-    date_of_birth = db.Column(db.DateTime)
+    birthday = db.Column(db.DateTime)
     health_status = db.Column(db.Boolean, default=False)
 
     __mapper_args__ = {
@@ -25,8 +25,8 @@ class Customer(User):
     def set_lastname(self, name):
         self.lastname = name
 
-    def set_date_of_birth(self, dateBirth):
-        self.date_of_birth = dateBirth
+    def set_birthday(self, birthday):
+        self.birthday = birthday
 
     def set_health_status(self, status):
         self.health_status = status
