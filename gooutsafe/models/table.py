0ac28fa4789b26983965423d405ccc3e7fa2dc07
Claire-gip <chiara_boni@outlook.it>
2020-10-27 21:49:51 +0100

    table properties

--- gooutsafe/models/table.py
+++ gooutsafe/models/table.py
@@ -6,13 +6,30 @@ from gooutsafe import db
 class Table(db.Model):
     __tablename__ = 'Table'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('Restaurant.id'))
-    restaurant = relationship('Restaurant', foreign_keys='Table.restaurant_id')
-    capacity = db.Column(db.Integer)
-    reservation_id = db.Column(db.Integer, db.ForeignKey('Reservation.id'))
-    reservations = relationship("Reservation", foreign_keys='Table.reservation_id')
+    __id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    __restaurant_id = db.Column(db.Integer, db.ForeignKey('Restaurant.id'))
+    __restaurant = relationship('Restaurant', foreign_keys='Table.restaurant_id')
+    __capacity = db.Column(db.Integer)
+    __reservation_id = db.Column(db.Integer, db.ForeignKey('Reservation.id'))
+    __reservations = relationship("Reservation", foreign_keys='Table.reservation_id')
 
     def __init__(self, capacity, restaurant):
-        self.capacity = capacity
-        self.restaurant = restaurant
+        self.__capacity = capacity
+        self.__restaurant = restaurant
+
+    def get_id(self):
+        return self.__id
+
+    def get_restaurant_id(self):
+        return self.__restaurant_id
+
+    
+    def get_capacity(self):
+        return self.__capacity
+    
+    def set_capacity(self, capacity):
+        self.__capacity = capacity
+        
+
+    def get_reservation_id(self):
+        return self.__reservation_id
