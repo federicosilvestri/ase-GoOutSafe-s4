827c52a812464027db2afb78bc2e6b8f0bb8bcd9
gasparax <nunziolopardo@live.it>
2020-10-29 10:14:06 +0100

    Added DAO classes for restaurant, table and reservation

--- gooutsafe/dao/Manager.py
+++
@@ -1,7 +0,0 @@
-
-class Manager(object):
-    @staticmethod
-    def check_none(**kwargs):
-        for name, arg in zip(kwargs.keys(), kwargs.values()):
-            if arg is None:
-                raise ValueError('You can\'t set %s argument to None' % name)
