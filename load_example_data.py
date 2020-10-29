961094601f6d78f77803938b8976b4ec49269901
gasparax <nunziolopardo@live.it>
2020-10-29 13:03:31 +0100

    Added health auth mock and loader

---
+++ load_example_data.py
@@ -0,0 +1,36 @@
+from gooutsafe import create_app
+import json
+
+
+PATH_HEALTH_AUTH_DATA = 'example_data/health_authority.json'
+
+
+def load_example_data():
+    create_app('config.BaseConfig')
+    from gooutsafe import db
+    from gooutsafe.models import health_authority
+    example_health_auth = load_health_auth_data(health_authority)
+    #TODO change the query with DAO class health_authority_manager
+    db.session.add(example_health_auth)
+    db.session.commit()
+
+  
+def load_health_auth_data(health_authority):
+    dict_health_auth = {}
+    with open(PATH_HEALTH_AUTH_DATA) as json_file:
+        dict_health_auth = json.load(json_file)
+    email = dict_health_auth["email"]
+    password = dict_health_auth["password"]
+    name = dict_health_auth["name"]
+    city = dict_health_auth["city"]
+    address = dict_health_auth["address"]
+    phone = dict_health_auth["phone"]
+    new_health_authority = health_authority.Authority(email=email, password=password, name=name,
+                              city=city,
+                              address=address, phone=phone
+                              )
+    return new_health_authority
+
+
+if __name__ == "__main__":  
+    load_example_data()
