4bf941907c9bea34096363154054d7d5dd22b2a5
gasparax <nunziolopardo@live.it>
2020-10-30 22:20:29 +0100

    Changed login button label

--- tests/dao/restaurant_availability_manager_test.py
+++ tests/dao/restaurant_availability_manager_test.py
@@ -44,18 +44,4 @@ class RestaurantAvailabilityManager(DaoTest):
         # test update
         for avas in rests_ava:
             for ava in avas:
-<<<<<<< HEAD
-                self.ram.delete_availabilitie(ava)
-
-    def test_retrieve_by_restaurant_id(self):
-        restaurant, _ = TestRestaurant.generate_random_restaurant()
-        self.re_ma.RestaurantManager.create_restaurant(restaurant)
-        self.re_ma.RestaurantManager.create_restaurant(restaurant)
-        start_time, end_time = TestRestaurantAvailability.generate_correct_random_times()
-        ava1 = self.ava.RestaurantAvailability(restaurant.id, start_time, end_time)
-        self.ram.create_availability(ava1)
-        ava2 = self.ram.retrieve_by_restaurant_id(restaurant.id)
-        TestRestaurantAvailability.assertEqualAvailability(ava1, ava2)
-=======
                 self.ram.delete_availability(ava)
->>>>>>> 7ef3385c3f26115ef3d5194b1220c0aa33b3ae9c
