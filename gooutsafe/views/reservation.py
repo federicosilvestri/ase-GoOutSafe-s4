116dccb27f0d5d052817299ce428074b4ecf7a3c
gasparax <nunziolopardo@live.it>
2020-10-30 16:55:51 +0100

    fixed reservation view

--- gooutsafe/views/reservation.py
+++ gooutsafe/views/reservation.py
@@ -6,6 +6,7 @@ from gooutsafe.models.restaurant import Restaurant
 from gooutsafe.models.reservation import Reservation
 from gooutsafe.dao.reservation_manager import ReservationManager
 from gooutsafe.dao.restaurant_manager import RestaurantManager
+from gooutsafe.dao.table_manager import TableManager
 from gooutsafe.forms.reservation import ReservationForm
 
 reservation = Blueprint('reservation', __name__)
@@ -15,21 +16,24 @@ reservation = Blueprint('reservation', __name__)
 @login_required
 def create_reservation(restaurant_id):
     form = ReservationForm()
-
     if request.method == 'POST':
-        if form.validate_on_submit():
-            user = current_user
-            #table=
-            restuarant = RestaurantManager.retrieve_by_id(restaurant_id)
+        if form.is_submitted():
+            print("Quel cane di Federico forti")
+            restaurant = RestaurantManager.retrieve_by_id(restaurant_id)
             start_data = form.data['start_date']
-            start_time = form.data['start_time']
             people_number = form.data['people_number']
-            reservation = Reservation(user, table, restaurant, people_number, start_data)
+            table = get_free_table(restaurant,people_number)
+            reservation = Reservation(current_user, table, restaurant, people_number, start_data)
             ReservationManager.create_reservation(reservation)
 
             return redirect('/reservations/' + str(restaurant_id) + '/' + str(reservation.id))
 
     return render_template('create_reservation.html', form=form)
+
+def get_free_table(restaurant, people_number):
+    tables = TableManager.retrieve_by_restaurant_id(restaurant.id).order_by(table.capacity)
+    print(table)
+    return table[0]
 
 
 @reservation.route('<restaurant_id>/delete_reservation/<int:id>')
@@ -40,7 +44,7 @@ def delete_reservation():
 @reservation.route('reservations/<restaurant_id>/<reservation_id>')
 def reservation_details(restaurant_id, reservation_id):
     reservation = db.session.query(Reservation).filter_by(id=int(reservation_id)).all()[0]
-    user = reservatio.user
+    user = reservation.user
     table = reservation.table
     restaurant = reservation.restaurant
     return render_template("reservation_details.html", reservation = reservation, user = user, table = table, restaurant = restaurant)
