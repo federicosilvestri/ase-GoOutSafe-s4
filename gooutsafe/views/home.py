0f79da11890399feeca225ed04f97ffbb7be8ac4
gasparax <nunziolopardo@live.it>
2020-10-31 23:45:22 +0100

    Added search bar in the home page

--- gooutsafe/views/home.py
+++ gooutsafe/views/home.py
@@ -1,16 +1,36 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request
+from flask_login import login_required
 
 from gooutsafe import db
 from gooutsafe.auth import current_user
 from gooutsafe.models.restaurant import Restaurant
+from gooutsafe.dao.restaurant_manager import RestaurantManager
+from gooutsafe.forms.home import HomeForm
+
 
 home = Blueprint('home', __name__)
 
 
-@home.route('/')
+@home.route('/', methods=['GET', 'POST'])
 def index():
-    if current_user is not None and hasattr(current_user, 'id'):
-        restaurants = db.session.query(Restaurant)
-    else:
-        restaurants = None
-    return render_template("index.html", restaurants=restaurants)
+    form = HomeForm()
+    restaurants=[]
+    if request.method == 'POST':
+        if form.is_submitted():
+            search_field = form.data['search_field']
+            search_filter = form.data['filters']
+            restaurants = search_by(search_field, search_filter)
+        return render_template("index.html", restaurants = restaurants, form = form)
+    
+    return render_template("index.html", form = form)
+
+def search_by(search_field, search_filter):
+    if search_filter == "Name":
+        restaurants = RestaurantManager.retrieve_by_restaurant_name(search_field)
+        return restaurants
+    if search_filter == "City":
+        restaurants = RestaurantManager.retrieve_by_restaurant_city(search_field)
+        return restaurants
+
+
+    
--- migrations/versions/35c56882daf3_.py
+++ migrations/versions/35c56882daf3_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 6b5f4286e76c
+Revision ID: 35c56882daf3
 Revises: 
-Create Date: 2020-10-31 16:57:58.569677
+Create Date: 2020-10-31 18:51:21.491535
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '6b5f4286e76c'
+revision = '35c56882daf3'
 down_revision = None
 branch_labels = None
 depends_on = None
