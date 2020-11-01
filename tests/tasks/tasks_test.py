db2b459f78b7d92950abcf94262bd514159543f1
LeoCal4 <linkindayd@gmail.com>
2020-11-01 12:49:12 +0100

    Created base test class for tasks

---
+++ tests/tasks/tasks_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class TasksTest(unittest.TestCase):
+    """
+    This class should be implemented by
+    all classes that tests models
+    """
+
+    def setUp(self):
+        from gooutsafe import create_app
+        create_app('config.TestConfig')
