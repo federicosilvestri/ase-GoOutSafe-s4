8ff69454ce31372bf0002b1ccdcfa6f1d72738d8
Paolo Murgia <62264946+Musca23@users.noreply.github.com>
2020-11-03 23:25:08 +0100

    added tests for roles

--- tests/models/test_role.py
+++ tests/models/test_role.py
@@ -15,8 +15,19 @@ class TestRole(ModelTest):
 
     def test_role_init(self):
         for _ in range(0,10):
-            pass
+            role, name = TestRole.generate_random_role()
+            self.assertEqual(role.name, name)
+
+    @staticmethod
+    def assertEqualRoles(r1, r2):
+        r = unittest.FunctionTestCase(TestRole)
+        r.assertEqual(r1.name, r2.name)
 
     @staticmethod
     def generate_random_role():
-        pass
+        from gooutsafe.models import Role
+
+        role_name = TestRole.faker.job()
+        role = Role(name=role_name)
+
+        return role, role_name
