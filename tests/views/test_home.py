ee8ba0a2e397f1919a76cebf20de3563308e3069
Paolo Murgia <62264946+Musca23@users.noreply.github.com>
2020-11-05 18:35:22 +0100

    added test home views

--- tests/views/test_home.py
+++ tests/views/test_home.py
@@ -1,8 +1,11 @@
 from .view_test import ViewTest
 from flask import template_rendered
+from faker import Faker
 import unittest
 
 class TestHome(ViewTest):
+
+    faker = Faker('it_IT')
 
     @classmethod
     def setUpClass(cls):
@@ -17,6 +20,23 @@ class TestHome(ViewTest):
         assert rv.status_code == 200
 
     def test_search_restaurant(self):
-        data = dict(keyword='ciao', filters='Name')
-        rv = self.client.get('/search', data=data)
+        #search restaurant with name filter
+        data = dict(keyword=TestHome.faker.company(), filters='Name')
+        rv = self.client.get('/search', query_string=data)
+        assert rv.status_code == 200
+        #search restaurant with city filter
+        data = dict(keyword=TestHome.faker.city(), filters='City')
+        rv = self.client.get('/search', query_string=data)
+        assert rv.status_code == 200
+        #search restaurant with menu filter
+        data = dict(keyword=TestHome.faker.country(), filters='Menu Type')
+        rv = self.client.get('/search', query_string=data)
+        assert rv.status_code == 200
+        #search restaurants with no option for the filters
+        data = dict(keyword=None, filters='Menu Type')
+        rv = self.client.get('/search', query_string=data)
+        assert rv.status_code == 200
+        #search restaurants with no filters
+        data = dict(keyword=TestHome.faker.company(), filters=None)
+        rv = self.client.get('/search', query_string=data)
         assert rv.status_code == 200
