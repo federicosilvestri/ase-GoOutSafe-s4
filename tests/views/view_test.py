6e1acd0e842dc6d4ffc5952dd6f04a64ce7c8dc2
Paolo Murgia <62264946+Musca23@users.noreply.github.com>
2020-11-06 00:26:33 +0100

    added test users view

--- tests/views/view_test.py
+++ tests/views/view_test.py
@@ -17,9 +17,17 @@ class ViewTest(unittest.TestCase):
         cls.test_customer = TestCustomer
         from gooutsafe.dao import customer_manager
         cls.customer_manager = customer_manager.CustomerManager
+        from models.test_operator import TestOperator
+        cls.test_operator = TestOperator
+        from gooutsafe.dao import operator_manager
+        cls.operator_manager = operator_manager.OperatorManager
+        from models.test_authority import TestAuthority
+        cls.test_authority = TestAuthority
+        from gooutsafe.dao import health_authority_manager
+        cls.authority_manager = health_authority_manager.AuthorityManager
 
-    #simulate the login for testing the views with @login_required
-    def login_test(self):
+    #simulate the customer login for testing the views with @login_required
+    def login_test_customer(self):
         customer, _ = self.test_customer.generate_random_customer()
         psw = customer.password
         customer.set_password(customer.password)
@@ -27,5 +35,17 @@ class ViewTest(unittest.TestCase):
         data = {'email': customer.email, 'password': psw}
         self.client.post('/login', data=data)
         return customer
+
+    #simulate the operator login for testing the views with @login_required
+    def login_test_operator(self):
+        operator, _ = self.test_operator.generate_random_operator()
+        psw = operator.password
+        operator.set_password(operator.password)
+        self.operator_manager.create_operator(operator=operator)
+        data = {'email': operator.email, 'password': psw}
+        self.client.post('/login', data=data)
+        return operator
+    
+        
 
     
